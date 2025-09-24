4c6f9c7d9ff0c61ac4e3161be78aaf13408ebd27
lawtrel <kazemarus@outlook.com>
2025-09-24 17:30:19 -0300

    criando o projeto

---
+++ apps/backend/src/server.js
@@ -0,0 +1,16 @@
+import express from 'express';
+import cors from 'cors';
+
+const app = express();
+const port = 3001;
+
+app.use(cors());
+app.use(express.json());
+
+app.get('/api/health', (req, res) => {
+  res.json({ status: `API do Frame 24 está no ar!`, time: new Date() });
+});
+
+app.listen(port, () => {
+  console.log(`🚀 API Frame 24 rodando em http://localhost:${port}`);
+});
